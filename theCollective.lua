3714d7ce5d48f23306cb2cebd7b910cc161bea87
Thomas <movecraft.pirates@gmail.com>
2022-05-24 17:17:52 +0200

    trying out something

---
+++ theCollective.lua
@@ -0,0 +1,16 @@
+local modules = peripheral.find("neuralInterface")
+
+if not modules then error("Must have a neural interface", 0) end
+if not modules.hasModule("plethora:introspection") then error("Must have an introspection module", 0) end
+if not modules.hasModule("plethora:kinetic", 0) then error("Must have a kinetic agument", 0) end
+if not modules.hasModule("plethora:sensor", 0) then error("Must have a sensor agument", 0) end
+
+peripheral.find("modem", rednet.open)
+
+
+while true do
+    local data = {name = modules.getName(), inventory = modules.getInventory(), equipment = modules.getEquipment(), player = modules.getMetaOwner(), isWalking = modules.isWalking() }
+
+    rednet.send(3, data)
+    sleep(1)
+end
---
+++ theCollectiveMonitor.lua
@@ -0,0 +1,13 @@
+local monitor = peripheral.wrap("right")
+
+rednet.open("top")
+
+local function write(input, x, y)
+    local curX, curY = monitor.getCursorPos()
+    curX = x or curX
+    curY = y or curY 
+    
+    monitor.setCursorPos(curX, curY)
+    monitor.write(input)
+end
+
