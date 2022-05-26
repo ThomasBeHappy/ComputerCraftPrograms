bf28504590c6d856784eb6108c52c3c4e75b7e36
Thomas <movecraft.pirates@gmail.com>
2022-05-26 13:59:38 +0200

    I did the stupid

--- nuclearCraftFission.lua
+++ nuclearCraftFission.lua
@@ -3,9 +3,9 @@ local reactor = peripheral.wrap("nuclearcraft:fission_port_0")
 local maxEnergy = reactor.getEnergyCapacity()
 
 while true do
-    if (reactor.getEnergyStored / maxEnergy * 100) < 0.3 then
+    if (reactor.getEnergyStored() / maxEnergy * 100) < 0.3 then
         rs.setOutput("back", true)
-    elseif   (reactor.getEnergyStored / maxEnergy * 100) > 0.9 then
+    elseif   (reactor.getEnergyStored() / maxEnergy * 100) > 0.9 then
         rs.setOutput("back", false)
     end
 end
