199c1628d28367272e2ac699a7f1bdbafc8ec8df
Thomas <movecraft.pirates@gmail.com>
2022-06-02 18:41:20 +0200

    AAAAAAAAAAA AAAAAAAAAAAAAA AAAAAAAAAAAA

---
+++ baseAlarm.lua
@@ -0,0 +1,41 @@
+local tape = peripheral.find("tape_drive")
+
+if not tape then -- Check if there is a Tape Drive
+    error("Tapedrive not found",0)
+end
+
+local function wipe()
+    local k = tape.getSize()
+    tape.stop()
+    tape.seek(-k)
+    tape.stop()
+    tape.seek(-90000)
+    local s = string.rep("\xAA", 8192)
+    for i = 1, k + 8191, 8192 do
+        tape.write(s)
+    end
+    tape.seek(-k)
+    tape.seek(-90000)
+end
+
+
+local function play()
+    wipe()
+    tape.stop()
+    tape.seek(-tape.getSize()) -- go back to the start
+
+    local h = http.get("https://github.com/ThomasBeHappy/ComputerCraftPrograms/blob/main/siren.dfpwm", nil, true) -- write in binary mode
+    tape.write(h.readAll()) -- that's it
+    h.close()
+
+    tape.seek(-tape.getSize()) -- back to start again
+    tape.setSpeed(1)
+    while tape.getState() ~= "STOPPED" do
+      sleep(1)
+    end
+    tape.play()
+end
+
+while true do
+    play()
+end
--- facilityMonitor.lua
+++ facilityMonitor.lua
@@ -23,7 +23,7 @@ local doorCount = 15
 
 
 -- list of computer ID's, with doors
-local authorized = {{id = 8, doors = {1,3} }, {id = 7, doors = {2,4}} }
+local authorized = {{id = 8, doors = {2,4} }, {id = 7, doors = {1,3}}, {id = 21, doors = {10,11}}, {id = 20, doors = {8,9}}, {id = 19, doors = {7,6,5}} }
 
 local function drawMap()
     while true do
@@ -49,50 +49,50 @@ local function initDoors()
         doorStates[i] = {startPos = {x = 1, z = 1}, endPos = {x = 1, z = 1}, state = false}
     end
 
-    doorStates[1].startPos = {x = 35, z = 15}
-    doorStates[1].endPos = { x = 35, z = 13}
+    doorStates[1].startPos = {x = 34, z = 15}
+    doorStates[1].endPos = { x = 34, z = 13}
 
     doorStates[2].startPos = {x = 39, z = 15}
     doorStates[2].endPos = { x = 39, z = 13}
 
-    doorStates[3].startPos = {x = 35, z = 19}
-    doorStates[3].endPos = { x = 35, z = 21}
+    doorStates[3].startPos = {x = 34, z = 19}
+    doorStates[3].endPos = { x = 34, z = 21}
 
     doorStates[4].startPos = {x = 39, z = 19}
     doorStates[4].endPos = { x = 39, z = 21}
 
-    doorStates[5].startPos = {x = 1, z = 1}
-    doorStates[5].endPos = { x = 1, z = 1}
+    doorStates[5].startPos = {x = 15, z = 28}
+    doorStates[5].endPos = { x = 18, z = 28}
 
-    doorStates[6].startPos = {x = 1, z = 1}
-    doorStates[6].endPos = { x = 1, z = 1}
+    doorStates[6].startPos = {x = 27, z = 28}
+    doorStates[6].endPos = { x = 30, z = 28}
 
-    doorStates[7].startPos = {x = 1, z = 1}
-    doorStates[7].endPos = { x = 1, z = 1}
+    doorStates[7].startPos = {x = 19, z = 33}
+    doorStates[7].endPos = { x = 22, z = 33}
 
-    doorStates[8].startPos = {x = 1, z = 1}
-    doorStates[8].endPos = { x = 1, z = 1}
+    doorStates[8].startPos = {x = 35, z = 33}
+    doorStates[8].endPos = { x = 38, z = 33}
 
-    doorStates[9].startPos = {x = 1, z = 1}
-    doorStates[9].endPos = { x = 1, z = 1}
+    doorStates[9].startPos = {x = 45, z = 28}
+    doorStates[9].endPos = { x = 48, z = 28}
 
-    doorStates[10].startPos = {x = 1, z = 1}
-    doorStates[10].endPos = { x = 1, z = 1}
+    doorStates[10].startPos = {x = 67, z = 28}
+    doorStates[10].endPos = { x = 70, z = 28}
 
-    doorStates[11].startPos = {x = 1, z = 1}
-    doorStates[11].endPos = { x = 1, z = 1}
+    doorStates[11].startPos = {x = 67, z = 33}
+    doorStates[11].endPos = { x = 70, z = 33}
 
-    doorStates[12].startPos = {x = 1, z = 1}
-    doorStates[12].endPos = { x = 1, z = 1}
+    doorStates[12].startPos = {x = 55, z = 46}
+    doorStates[12].endPos = { x = 55, z = 48}
 
-    doorStates[13].startPos = {x = 1, z = 1}
-    doorStates[13].endPos = { x = 1, z = 1}
+    doorStates[13].startPos = {x = 55, z = 37}
+    doorStates[13].endPos = { x = 55, z = 39}
 
-    doorStates[14].startPos = {x = 1, z = 1}
-    doorStates[14].endPos = { x = 1, z = 1}
+    doorStates[14].startPos = {x = 59, z = 46}
+    doorStates[14].endPos = { x = 59, z = 48}
 
-    doorStates[15].startPos = {x = 1, z = 1}
-    doorStates[15].endPos = { x = 1, z = 1}
+    doorStates[15].startPos = {x = 59, z = 37}
+    doorStates[15].endPos = { x = 59, z = 39}
 
     for key, door in pairs(doorStates) do
         box:set_line(door.startPos.x, door.startPos.z, door.endPos.x, door.endPos.z, colors.red, 1)
@@ -104,18 +104,18 @@ local function initialize()
     -- 100 width, 52 height
     
     -- Elevator hallway
-    box:set_line(35, 10, 39, 10, colors.white, 1)
-    box:set_line(35, 10, 35, 28, colors.white, 1)
+    box:set_line(34, 10, 39, 10, colors.white, 1)
+    box:set_line(34, 10, 34, 28, colors.white, 1)
     box:set_line(39, 10, 39, 28, colors.white, 1)
     -- Hallway right
     box:set_line(39, 28, 80, 28, colors.white, 1)
     box:set_line(80, 33, 80, 28, colors.white, 1)
     box:set_line(80, 33, 59, 33, colors.white, 1)
     -- Hallway left
-    box:set_line(35, 28, 10, 28, colors.white, 1)
+    box:set_line(34, 28, 10, 28, colors.white, 1)
     box:set_line(10, 33, 10, 28, colors.white, 1)
     box:set_line(10, 33, 55, 33, colors.white, 1)
-    -- Hallway top
+    -- Hallway bottom
     box:set_line(55, 50, 55, 33, colors.white, 1)
     box:set_line(55, 50, 59, 50, colors.white, 1)
     box:set_line(59, 33, 59, 50, colors.white, 1)
@@ -137,7 +137,7 @@ local function receiveUpdate()
             if authorized[index].id == id then
 
                 for _index, value in ipairs(authorized[index].doors) do
-                    doorStates[_index].state = message.doors[_index]
+                    doorStates[authorized[index].doors[_index]].state = message.doors[_index]
                 end
 
             end
